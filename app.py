f84997655206732da0b60e965399688ca0326333
Kat Snider-Belinski <146659472+ksniderbel@users.noreply.github.com>
2024-02-05 11:30:48 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,73 @@
+# Import the dependencies.
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+# View all of the classes that automap found
+Base.classes.keys()
+print(Base.classes.keys())
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+# See below function
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/station<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt<br/>"
+    )
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+
+
+    session = Session(engine)
+    # For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+    start_tobs = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+    session.close
+    start_tobs_list = []
+    for min, max, avg in start_tobs:
+        start_dict = {}
+        start_dict["min"] = min
+        start_dict["max"] = max
+        start_dict["avg"] = avg
+        start_tobs_list.append(start_dict)
+    return jsonify(start_tobs_list)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
